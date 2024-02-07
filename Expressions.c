f5df2d8eb5cc6d713dc87901c48f10d34e60c7fa
gopikasambandam <106174946+gopikasambandam@users.noreply.github.com>
2024-02-07 16:33:57 +0530

    Expressions.c

---
+++ Expressions.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+  int a=20,b=15;
+  int add,mul,sub,divi,rem;
+  //Addition
+  add=a+b;
+  //Subtraction
+  sub=a-b;
+  //Multiplication
+  mul=a*b;
+  //Division
+  divi=a/b;
+  //Modulus
+  rem=a%b;
+  printf("%d\n%d\n%d\n%d\n%d\n",add,sub,mul,divi,rem);
+}
